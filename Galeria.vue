a3ceec1f0836b64b1a946bccdbdcd2a61856c000
lizethvaleria <71527663+lizethvaleria@users.noreply.github.com>
2020-10-16 13:17:20 -0500

    Initial commit

---
+++ Galeria.vue
@@ -0,0 +1,455 @@
+<template>
+  <div class="navbarPage">
+    <Navbar />
+    <!-- Page Content -->
+    <div class="navbarPage container">
+      <br />
+
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+          <router-link to="/">Inicio</router-link>
+        </li>
+        <li class="breadcrumb-item active">FIME3D</li>
+      </ol>
+      <br />
+      <!-- Intro Content -->
+      <div class="row">
+        <div class="col-lg-6">
+          <a href="#"
+            ><img
+              class="card-img-top"
+              src="./assets/img3d/fime3d_logo.jpg"
+              width="300"
+              height="450"
+              alt=""
+          /></a>
+        </div>
+        <div class="col-lg-6">
+          <h1 id="heading"><i class="fas fa-cube"></i> FIME3D</h1>
+          <p>
+            Nuestro equipo de FIME3D es el responsable de llevar acabo las
+            necesidades graficas en el campo de 3D utilizando los diversos
+            recursos del campo grafico digital,tradicional y artistico.
+          </p>
+          <p>
+            Nuestro proposito es ser un punto de aprendizaje en la aplicacion de
+            los recursos digitales tridimensionales,para poder preparar al
+            alumno con diversas tecnicas para el desarrollo de proyectos de alta
+            gama
+          </p>
+          <!-- Categories Widget -->
+          <br />
+          <br />
+          <div class="card my-4">
+            <h6 class="card-header">Redes sociales:</h6>
+            <div class="card-body">
+              <div class="row">
+                <div class="col-lg-4">
+                  <ul class="list-unstyled mb-0">
+                    <li>
+                      <a
+                        href="https://www.facebook.com/FIME3D"
+                        target="_blank"
+                      >
+                        <i class="fab fa-facebook-f"></i> Facebook</a
+                      >
+                    </li>
+                    <li>
+                      <a
+                        href="https://www.facebook.com/groups/443943476001442"
+                        target="_blank"
+                      >
+                        <i class="fab fa-grupo"></i>Grupo de Facebook</a
+                      >
+                    </li>
+                  </ul>
+                </div>
+                <div class="col-lg-4">
+                  <ul class="list-unstyled mb-0">
+                    <li>
+                      <a
+                        href="https://www.instagram.com/fime3d/?hl=es-la"
+                        target="_blank"
+                      >
+                        <i class="fab fa-instagram"></i> Instagram</a
+                      >
+                    </li>
+                    <li>
+                      <a
+                        href="https://www.youtube.com/channel/UCfmQiSfgZ5cMDe-kAYplmww/featured"
+                        target="_blank"
+                      >
+                        <i class="fab fa-youtube"></i> Youtube</a
+                      >
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Categories Widget -->
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <!-- /.row -->
+
+      <div align="center">
+        <h2>Proyectos Tridimensionales</h2>
+        <br />
+        <div class="row">
+          <div class="col-lg-6 portfolio-item">
+            <div class="card h-100">
+              <a href="#"
+                ><img class="card-img-top" src="./assets/img3d/1.jpg" alt=""
+              /></a>
+            </div>
+          </div>
+          <div class="col-lg-6 portfolio-item">
+            <div class="card h-100">
+              <a href="#"
+                ><img class="card-img-top" src="./assets/img3d/2.jpg" alt=""
+              /></a>
+            </div>
+          </div>
+          <div class="col-lg-6 portfolio-item">
+            <div class="card h-100">
+              <a href="#"
+                ><img class="card-img-top" src="./assets/img3d/7.jpg" alt=""
+              /></a>
+            </div>
+          </div>
+          <div class="col-lg-6 portfolio-item">
+            <div class="card h-100">
+              <a href="#"
+                ><img class="card-img-top" src="./assets/img3d/11.jpg" alt=""
+              /></a>
+            </div>
+          </div>
+
+          <div class="col-lg-6 portfolio-item">
+            <div class="card h-800">
+              <a href="#"
+                ><img class="card-img-top" src="./assets/img3d/10.jpg" alt=""
+              /></a>
+            </div>
+          </div>
+          <div class="col-lg-6 portfolio-item">
+            <div class="card h-100">
+              <a href="#"
+                ><img class="card-img-top" src="./assets/img3d/13.png" alt=""
+              /></a>
+            </div>
+          </div>
+          <div class="col-lg-6 portfolio-item">
+            <div class="card h-100">
+              <a href="#"
+                ><img class="card-img-top" src="./assets/img3d/14.png" alt=""
+              /></a>
+            </div>
+          </div>
+          <div class="col-lg-6 portfolio-item">
+            <div class="card h-100">
+              <a href="#"
+                ><img class="card-img-top" src="./assets/img3d/15.png" alt=""
+              /></a>
+            </div>
+          </div>
+
+          <div class="col-lg-6 portfolio-item">
+            <div class="card h-100">
+              <a href="#"
+                ><img class="card-img-top" src="./assets/img3d/16.png" alt=""
+              /></a>
+            </div>
+          </div>
+          <div class="col-lg-6 portfolio-item">
+            <div class="card h-100">
+              <a href="#"
+                ><img class="card-img-top" src="./assets/img3d/17.png" alt=""
+              /></a>
+            </div>
+          </div>
+          <div class="col-lg-6 portfolio-item">
+            <div class="card h-100">
+              <a href="#"
+                ><img class="card-img-top" src="./assets/img3d/18.jpeg" alt=""
+              /></a>
+            </div>
+          </div>
+          <div class="col-lg-6 portfolio-item">
+            <div class="card h-100">
+              <a href="#"
+                ><img class="card-img-top" src="./assets/img3d/19.png" alt=""
+              /></a>
+            </div>
+          </div>
+
+          <div class="col-lg-6 portfolio-item">
+            <div class="card h-400">
+              <a href="#"
+                ><img class="card-img-top" src="./assets/img3d/20.jpg" alt=""
+              /></a>
+            </div>
+          </div>
+          <div class="col-lg-6 portfolio-item">
+            <div class="card h-100">
+              <a href="#"
+                ><img class="card-img-top" src="./assets/img3d/21.png" alt=""
+              /></a>
+            </div>
+          </div>
+          <div class="col-lg-6 portfolio-item">
+            <div class="card h-100">
+              <a href="#"
+                ><img class="card-img-top" src="./assets/img3d/22.png" alt=""
+              /></a>
+            </div>
+          </div>
+          <div class="col-lg-6 portfolio-item">
+            <div class="card h-100">
+              <a href="#"
+                ><img class="card-img-top" src="./assets/img3d/23.png" alt=""
+              /></a>
+            </div>
+          </div>
+          <div class="col-lg-6 portfolio-item">
+            <div class="card h-100">
+              <a href="#"
+                ><img class="card-img-top" src="./assets/img3d/24.png" alt=""
+              /></a>
+            </div>
+          </div>
+          <div class="col-lg-6 portfolio-item">
+            <div class="card h-400">
+              <a href="#"
+                ><img class="card-img-top" src="./assets/img3d/25.png" alt=""
+              /></a>
+            </div>
+          </div>
+          <div class="col-lg-6 portfolio-item">
+            <div class="card h-100">
+              <a href="#"
+                ><img class="card-img-top" src="./assets/img3d/26.png" alt=""
+              /></a>
+            </div>
+          </div>
+          <div class="col-lg-6 portfolio-item">
+            <div class="card h-100">
+              <a href="#"
+                ><img class="card-img-top" src="./assets/img3d/27.png" alt=""
+              /></a>
+            </div>
+          </div>
+
+          <div class="col-lg-6 portfolio-item">
+            <div class="card h-100">
+              <a href="#"
+                ><img class="card-img-top" src="./assets/img3d/28.png" alt=""
+              /></a>
+            </div>
+          </div>
+          <div class="col-lg-6 portfolio-item">
+            <div class="card h-100">
+              <a href="#"
+                ><img class="card-img-top" src="./assets/img3d/29.jpg" alt=""
+              /></a>
+            </div>
+          </div>
+          <div class="col-lg-6 portfolio-item">
+            <div class="card h-100">
+              <a href="#"
+                ><img class="card-img-top" src="./assets/img3d/30.png" alt=""
+              /></a>
+            </div>
+          </div>
+          <div class="col-lg-6 portfolio-item">
+            <div class="card h-100">
+              <a href="#"
+                ><img class="card-img-top" src="./assets/img3d/31.jpeg" alt=""
+              /></a>
+            </div>
+          </div>
+
+          <div class="col-lg-6 portfolio-item">
+            <div class="card h-100">
+              <a href="#"
+                ><img class="card-img-top" src="./assets/img3d/32.jpeg" alt=""
+              /></a>
+            </div>
+          </div>
+          <div class="col-lg-6 portfolio-item">
+            <div class="card h-100">
+              <a href="#"
+                ><img class="card-img-top" src="./assets/img3d/33.jpeg" alt=""
+              /></a>
+            </div>
+          </div>
+          <div class="col-lg-6 portfolio-item">
+            <div class="card h-100">
+              <a href="#"
+                ><img class="card-img-top" src="./assets/img3d/34.png" alt=""
+              /></a>
+            </div>
+          </div>
+          <div class="col-lg-6 portfolio-item">
+            <div class="card h-100">
+              <a href="#"
+                ><img class="card-img-top" src="./assets/img3d/35.png" alt=""
+              /></a>
+            </div>
+          </div>
+
+          <div class="col-lg-6 portfolio-item">
+            <div class="card h-100">
+              <a href="#"
+                ><img class="card-img-top" src="./assets/img3d/36.png" alt=""
+              /></a>
+            </div>
+          </div>
+          <div class="col-lg-6 portfolio-item">
+            <div class="card h-100">
+              <a href="#"
+                ><img class="card-img-top" src="./assets/img3d/37.png" alt=""
+              /></a>
+            </div>
+          </div>
+          <div class="col-lg-6 portfolio-item">
+            <div class="card h-200">
+              <a href="#"
+                ><img class="card-img-top" src="./assets/img3d/38.png" alt=""
+              /></a>
+            </div>
+          </div>
+          <div class="col-lg-6 portfolio-item">
+            <div class="card h-100">
+              <a href="#"
+                ><img class="card-img-top" src="./assets/img3d/39.jpeg" alt=""
+              /></a>
+            </div>
+          </div>
+
+          <!-- /.row -->
+        </div>
+
+        <!-- /.container -->
+
+        <!-- Footer -->
+        <footer class="page-footer font-small blue py-4">
+          <!-- Footer Links -->
+          <div class="container text-center text-md-left">
+            <!-- Grid row -->
+            <div class="row">
+              <!-- Grid column -->
+              <div class="col-md-6 mt-md-0 mt-3">
+                <!-- Content -->
+                <h5 class="text-uppercase">Acerca de nosotros</h5>
+                <p>
+                  <i class="fas fa-map-marked"></i> &nbsp; Av. Universidad S/N,
+                  Ciudad Universitaria<br />
+                  San Nicolás de los Garza, N. L., C.P. 66455
+                </p>
+                <p>
+                  <i class="fas fa-phone-volume"></i>&nbsp;(52) 81 8329 4020
+                </p>
+                <p>
+                  <i class="fas fa-mail-bulk"></i>&nbsp;contacto@fime.uanl.mx
+                </p>
+              </div>
+              <!-- Grid column -->
+              <hr class="clearfix w-100 d-md-none pb-3" />
+              <!-- Grid column -->
+              <div class="col-md-3 mb-md-0 mb-3">
+                <!-- Links -->
+                <h5 class="text-uppercase">UANL</h5>
+                <ul class="list-unstyled">
+                  <li>
+                    <a href="https://www.uanl.mx/enlinea/" target="_blank"
+                      ><i class="fas fa-university"></i> Servicios en línea</a
+                    >
+                  </li>
+                  <li>
+                    <a
+                      href="https://plataformanexus.uanl.mx/#/Login"
+                      target="_blank"
+                    >
+                      <i class="fas fa-id-card-alt"></i> Nexus 7</a
+                    >
+                  </li>
+                  <li>
+                    <a href="https://www.uanl.mx/" target="_blank">
+                      <i class="fas fa-building"></i> UANL</a
+                    >
+                  </li>
+                </ul>
+              </div>
+
+              <div class="col-md-3 mb-md-0 mb-3">
+                <h5 class="text-uppercase">Redes Sociales</h5>
+
+                <ul class="list-unstyled">
+                  <li>
+                    <a
+                      href="https://www.facebook.com/fime.oficial/"
+                      target="_blank"
+                    >
+                      <i class="fab fa-facebook-f"></i> Facebook</a
+                    >
+                  </li>
+                  <li>
+                    <a
+                      href="https://twitter.com/fime_oficial?lang=es"
+                      target="_blank"
+                    >
+                      <i class="fab fa-twitter"></i> Twitter</a
+                    >
+                  </li>
+                  <li>
+                    <a
+                      href="https://www.instagram.com/fime.oficial/?hl=es-la"
+                      target="_blank"
+                    >
+                      <i class="fab fa-instagram"></i> Instagram</a
+                    >
+                  </li>
+                  <li>
+                    <a
+                      href="https://www.youtube.com/channel/UCfmQiSfgZ5cMDe-kAYplmww/featured"
+                      target="_blank"
+                    >
+                      <i class="fab fa-youtube"></i> Youtube</a
+                    >
+                  </li>
+                </ul>
+              </div>
+              <!-- Grid column -->
+            </div>
+            <!-- Grid row -->
+          </div>
+          <!-- Footer Links -->
+
+          <!-- Copyright -->
+          <div class="footer-copyright text-center py-3">
+            Copyright © 2020
+            <a href="https://www.fime.uanl.mx/" target="_blank"
+              ><i class="fas fa-paw"></i> FIME</a
+            >
+          </div>
+          <!-- Copyright -->
+        </footer>
+        <!-- Footer -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from "./components/Navbar";
+export default {
+  name: "Galeria",
+  components: { Navbar },
+};
+</script>
